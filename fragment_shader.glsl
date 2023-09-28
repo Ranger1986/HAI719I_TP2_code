608a2f03c97a202bf4ebd85284f5992321f21476
pierre <pierre6richard7@gmail.com>
2023-09-28 17:24:57 +0200

    Q3

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -2,6 +2,7 @@
 
 // Output data
 out vec4 FragColor;
+in vec3 o_color;
 // Ajouter une variable interpolée o_color venant du vertex shader
 //
 
@@ -9,6 +10,6 @@ void main()
 {
 
     // Mettre à jour la couleur avec la variable interpolée venant du vertex shader
-    FragColor = vec4(1., 0., 0., 1.);// Output color = red
+    FragColor = vec4(o_color, 1.);// Output color = red
 
 }
